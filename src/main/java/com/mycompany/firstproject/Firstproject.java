f63512a4df8c3ea1185133e9360b5462604f6b8d
RC_Student_lab <RC_Student_lab@10.0.0.72>
2024-09-09 08:32:06 +0000

    created new program adding numbers

--- src/main/java/com/mycompany/firstproject/Firstproject.java
+++ src/main/java/com/mycompany/firstproject/Firstproject.java
@@ -11,6 +11,9 @@ package com.mycompany.firstproject;
 public class Firstproject {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int a=5;
+        int b=10;
+        int c=a+b;
+        System.out.println("sum:" +c);
     }
 }
